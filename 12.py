625ad788148104ca6f5c7038f139134f10be21dc
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-13 14:58:14 +0530

    DAA lab

---
+++ 12.py
@@ -0,0 +1,22 @@
+# write a python program to find the greatest of three numbers and also write the
+#algorithm
+
+import time
+s=time.time();
+num1=float(input("enter the first number: "));
+num2=float(input("enter the second number: "));
+num3=float(input("enter the third number: "));
+
+
+if num1>=num2 and num1>=num3:
+    print(f"{num1} is the largest number");
+
+elif num2>=num1 and num2>=num3:
+    print(f"{num2} is the largest number");
+    
+else:
+    print(f"{num3} is the largest number");
+    
+e=time.time();
+
+print(e-s);
