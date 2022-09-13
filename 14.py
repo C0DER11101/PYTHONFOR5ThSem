1d25f084297b5cd1bc6a3f39c7f5c4cc51019508
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-13 14:58:46 +0530

    DAA lab

---
+++ 14.py
@@ -0,0 +1,18 @@
+#Write a program in PYTHON to sum the series 1/2 + 2/3 +..........+n/n+1. Also write the
+#algorithm and calculate the worst case complexity and space complexity.
+
+import time
+num=int(input("enter num: "));
+
+s=time.time();
+
+suM=float();
+
+for n in range(1, num+1):
+    suM+=n/(n+1);
+    
+
+e=time.time();
+
+print(f"Sum is {suM}\n\n");
+print(e-s);
