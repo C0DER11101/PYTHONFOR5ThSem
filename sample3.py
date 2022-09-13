f181d4b0674a70777f34bc01858b3af58a8ec0ac
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-13 07:06:58 +0530

    sample

---
+++ sample3.py
@@ -0,0 +1,17 @@
+a=[]
+a.append([int(j) for j in input().split()])
+
+b=[int(j) for j in input().split()];
+
+c=input().split();
+
+d=[str(j) for j in input().split()];
+
+
+print(d);
+
+print(c);
+
+print(b);
+
+print(a);
