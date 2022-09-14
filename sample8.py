36d0a3cd054bfccf19c7acce4bd7b2233cc8029e
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-14 09:38:54 +0530

    Dictionaries in Python

---
+++ sample8.py
@@ -0,0 +1,14 @@
+# PYTHON DICTIONARIES
+
+D={1:'one',
+   2:'two',
+   3:'three',
+   4:'four',
+   5:'five'
+   };
+
+print(D[1]);  # prints the corresponding value of 1 in dictionary D i.e. 'one'
+print(D[5]);  # prints the corresponding value of 5 in dictionary D i.e. 'five'
+
+print(D);  # prints the dictionary D
+# OUTPUT: {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five'}
