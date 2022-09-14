5e3b83f7a251af2cae43f5bd390b89098e3cff3d
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-14 11:36:56 +0530

    Keywords in Python

---
+++ KeywordsInPython.py
@@ -0,0 +1,24 @@
+# KEYWORDS IN PYTHON:
+
+# import keyword
+
+# print(keyword.kwlist); # shows list of keywords in Python!!
+
+# True, False, None
+
+print(True==1);
+print(False==0);
+print(True==0);
+print(False==1);
+
+print("\nNone\n");
+print(None==0);
+print(None==1);
+
+""" 
+None:
+    This is a special constant used 
+    to denote a null value or a void. It’s important to remember, 0, 
+    any empty container(e.g empty list) does not compute to None. 
+    It is an object of its datatype.
+"""
