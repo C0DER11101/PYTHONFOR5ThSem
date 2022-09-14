71ce086a7bf6d6e4229b690884b1f7f01091d78d
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-14 11:37:16 +0530

    More on keywords in Python

---
+++ KeywordsInPython_PT2.py
@@ -0,0 +1,10 @@
+# KEYWORDS IN PYTHON
+
+print(True+False); # output: 1
+print(True+False+True); # output: 2
+print(False+False+True); # output: 1
+
+print(True+True); # output: 2(True+True)  since: True is same as 1
+
+print(True*False); # output: 0
+print(True*True*False); # output: 0
