c8cfd0e610c3edde477f0a4ee67ddb066db04571
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-16 20:34:17 +0530

    DAA Lab Exercise1

---
+++ DAALabExercise1.py
@@ -0,0 +1,8 @@
+# Write a program in python to find whether a number is odd or even. Also write the algorithm
+a=int(input("enter a number: "));
+
+if a%2==0:
+    print(f"{a} is even!!\n\n");
+    
+else:
+    print(f"{a} is odd!!\n\n");
