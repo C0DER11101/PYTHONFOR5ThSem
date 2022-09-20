60853fc2bde988d3566ae6cbae8462276616e3b4
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-09-20 11:56:08 +0530

    Typcasting in Python

---
+++ test2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 20 11:18:30 2022
+
+@author: priyanuj
+"""
+
+a=int()
+
+a=int(5/3);  # typcasting the result into integer!!
+
+print(a);
