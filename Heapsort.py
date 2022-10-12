e51ae530f9139b415c6f252ca1e9948e7aa76122
Priyanuj Bora <while.alive.program@gmail.com>
2022-10-12 11:46:31 +0530

    Heap Sort in Python

---
+++ Heapsort.py
@@ -0,0 +1,13 @@
+# heapsort in python!!!
+
+def heapSort():
+	return;
+
+array=list();
+size=int(input("enter size of the array: "));
+
+print("enter elements into the array:\n\n");
+
+for i in range(size):
+	el=int(input(": "));
+	array.append(el);
