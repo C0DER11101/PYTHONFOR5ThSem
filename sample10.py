224a09b7e3eab44efff18599c734880584559d59
Priyanuj Bora <while.alive.program@gmail.com>
2022-10-12 06:36:53 +0530

    Just a test

---
+++ sample10.py
@@ -0,0 +1,18 @@
+# test program:
+
+arr=list();
+
+size=int(input("enter size of the array: "));
+
+print("enter elements into the array: ");
+
+for i in range(size):
+	el=int(input(":"));
+	arr.append(el);
+
+mid=len(arr)//2;
+
+print(f"Array: {arr}");
+print(f"Mid = {mid}");
+print(arr[:mid]);
+print(arr[mid:]);
