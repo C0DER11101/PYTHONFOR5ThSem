729491e06ff4e80a616af1baff5e76302892a136
Priyanuj Bora <while.alive.program@gmail.com>
2022-11-02 19:24:00 +0530

    input().split()

--- sample3.py
+++ sample3.py
@@ -1,17 +1,26 @@
 a=[]
+
+print("\nenter elements into list a:\n");
 a.append([int(j) for j in input().split()])
 
+print("\nenter elements into list b:\n");
 b=[int(j) for j in input().split()];
 
+print("\nenter elements into list c:\n");
 c=input().split();
 
+print("\nenter elements into list d:\n");
 d=[str(j) for j in input().split()];
 
 
+print("\nlist-d:\n");
 print(d);
 
+print("\nlist-c:\n");
 print(c);
 
+print("\nlist-b:\n");
 print(b);
 
+print("\nlist-a:\n");
 print(a);
