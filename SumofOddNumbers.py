7b774bf6fda8868a958729b1040f8e427027a264
Priyanuj Bora <while.alive.program@gmail.com>
2022-11-02 14:25:25 +0530

    Sum of odd numbers between 1 and 20

---
+++ SumofOddNumbers.py
@@ -0,0 +1,11 @@
+# Sum of odd numbers between 1 and 120
+
+sumOdd=0;
+
+for i in range(1, 20):
+    if i%2!=0:
+        sumOdd+=i;
+
+#end of if
+
+print("\nSum of odd numbers between 1 and 20 is: ", sumOdd);
