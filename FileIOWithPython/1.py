2557ee51ab42769c9b8d2e689cdabda3077c7596
Priyanuj Bora <while.alive.program@gmail.com>
2022-11-03 23:30:12 +0530

    File I/O with Python

--- FileIOWithPython/1.py
+++ FileIOWithPython/1.py
@@ -4,4 +4,17 @@ f=open("hello.txt");
 
 content=f.read();
 
+f.close();
 print(content);
+
+f=open("hello.txt");
+
+for line in f:
+    print(line);
+    pass
+
+f.close();
+
+f=open("hello.txt");
+print(f.readlines());
+f.close();
