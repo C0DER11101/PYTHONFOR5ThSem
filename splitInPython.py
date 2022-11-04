81b469c79b5ac9f4d6b5c86292564ffd2dba352e
Priyanuj Bora <while.alive.program@gmail.com>
2022-11-04 13:50:12 +0530

    Split() in Python

---
+++ splitInPython.py
@@ -0,0 +1,17 @@
+# split() in python!!
+
+s1="Welcome to Python";
+print(s1);
+
+s2=s1.split();
+print(s2);
+
+s3=s1.split(' ');
+print(s3);
+
+s4="Welcome-to-Python";
+print(s4);
+
+s5=s4.split('-');
+
+print(s5);
