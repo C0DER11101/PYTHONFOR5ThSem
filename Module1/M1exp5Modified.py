6660da4e572fbe99f6acfad776ffed32a242e7b4
Priyanuj Bora <while.alive.program@gmail.com>
2022-11-15 23:53:00 +0530

    Substring program modified

---
+++ Module1/M1exp5Modified.py
@@ -0,0 +1,31 @@
+string=input("enter a string: ");
+
+substr=input("enter a substring: ");
+
+
+i=0;
+j=0;
+
+isSubstr=False;
+contiguous=0;
+
+while i<len(string):
+    if string[i]==substr[j]:
+        j+=1;
+        pass;
+    else:
+        j=0;
+        pass;
+    i+=1;
+
+    if j==len(substr):
+        break;
+
+    pass;
+
+if j==len(substr):
+    print(f"{substr} is in {string}\n");
+    pass;
+else:
+    print(f"{substr} is not in {string}\n");
+    pass;
